aa5b6a4d1970fd0e7822e3990fca8722adbb491f
giuse <giuseppedefilippo03@gmail.com>
2024-12-12 11:09:06 +0100

    aggiustato app.jsx al posto di richiamare le funzioni

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,9 @@ function App() {
       <div className="">
         <h1 className="backgroundgreen p-2"><span className="m-3">task Manager</span></h1>
         <h3 className="ms-4">Current Task(4)</h3>
-        <ul className="">{ElementsArrayComponents()}</ul>
+        <ul className=""><ElementsArrayComponents /></ul>
         <h3 className="ms-4 borderTop">Completed Task(6)</h3>
-        <ul className="">{ElementiArrayCompletedComponents()}</ul>
+        <ul className=""><ElementiArrayCompletedComponents /></ul>
       </div>
     </>
   )
